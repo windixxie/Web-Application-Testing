147f7785cc3f48cfd5dee907868b4654b3d114e9
Winnie Song <winniesong0121@gmail.com>
2019-10-03 18:31:18 -0400

    tests written for Display

--- client/src/components/Display.spec.js
+++ client/src/components/Display.spec.js
@@ -1,17 +1,38 @@
 import React from "react";
-import { render, getByTestId } from "react-testing-library";
+import { render, fireEvent } from "react-testing-library";
 import "jest-dom/extend-expect";
-import Display from "./Display";
+import Dashboard from "./Dashboard";
 
-describe("<Display/>", () => {
-    it("should render the display comp", () => {
-        const testState= {
-            player: "Winnie",
-            balls: 0,
-            strikes: 0
+describe("<Dashboard />", () => {
+    it("should render", () => {
+        const testState = {
+            ball: 0,
+            strike: 0,
+            foule: 0,
+            hit: 0
         };
-        const { getByTestId } = render(<Display atBat={testState} />);
-        const player = getByTestId(/player/i);
-        
+
+        const { getByTestId } = render(
+            <Dashboard
+                ball={testState.ball}
+                strike={testState.strike}
+                foul={testState.foul}
+                hit={testState.hit}
+            />
+        );
+
+        const ballBtn = getByTestId("ball");
+        const strikeBtn = getByTestId("strike");
+        const foulBtn = getByTestId("foul");
+        const hitBtn = getByTestId("hit");
+        fireEvent.click(ballBtn);
+        fireEvent.click(strikeBtn);
+        fireEvent.click(foulBtn);
+        fireEvent.click(hitBtn);
+
+        expect(ballBtn).toHaveTextContent(/ball/i);
+        expect(strikeBtn).toHaveTextContent(/strike/i);
+        expect(foulBtn).toHaveTextContent(/foul/i);
+        expect(hitBtn).toHaveTextContent(/hit/i);
     })
 })
